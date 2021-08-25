63dae9f8bf41a959510f8a589bccd46be42a960b
David <jyug129@gmail.com>
2021-08-25 02:12:22 -0700

    crawler as arapper

---
+++ crawler/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('express-async-errors');
+const checker = require('ikea-availability-checker');
+
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  const buCodes = JSON.parse(req.query.buCodes || '[]');
+  const productId = req.query.productId.toString();
+  console.log(buCodes, productId)
+  const results = await Promise.all(buCodes.map(async (buCode) => {
+    return await checker.availability(buCode.toString(), productId);
+  }));
+  res.status(200).send(results);
+});
+
+app.listen(3000, () => console.log('backend on port 3000!'));
